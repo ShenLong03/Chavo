08c1596204fbad636ac9c5b6cd99a36682e40fc1
ShenLong03 <47692181+ShenLong03@users.noreply.github.com>
2020-01-02 16:01:58 -0600

    Zona Privada Inicio
    Iniciamos con el desarrollo de la zona privada

---
+++ Chavo.ECommerce/Controllers/PrivateZoneController.cs
@@ -0,0 +1,25 @@
+﻿namespace Chavo.ECommerce.Controllers
+{
+    using Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class PrivateZoneController : Controller
+    {
+        private DataContextLocal db = new DataContextLocal();
+
+        public ActionResult Index()
+        {
+            return View(db.Customers.FirstOrDefault());
+        }
+
+        public ActionResult AssignedInvestor()
+        {
+            var customer = db.Customers.FirstOrDefault();
+            return View(db.Customers.FirstOrDefault());
+        }
+    }
+}
