0eebb8901e92f67abcf202aae9afca0f6cc31823
Donovan Jarquin <34379214+DonovanJarquin@users.noreply.github.com>
2020-01-28 08:33:46 -0600

    Listo para la contraseña sencilla

--- Chavo.ECommerce/Models/AccountViewModels.cs
+++ Chavo.ECommerce/Models/AccountViewModels.cs
@@ -88,14 +88,14 @@ namespace Chavo.ECommerce.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "El campo {0} al menos debe ser de {2} caracteres de largo.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
--- Chavo.Web/App_Start/IdentityConfig.cs
+++ Chavo.Web/App_Start/IdentityConfig.cs
@@ -60,10 +60,10 @@ namespace Chavo.Web
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                //RequireNonLetterOrDigit = true,
+                //RequireDigit = true,
+                //RequireLowercase = true,
+                //RequireUppercase = true,
             };
 
             // Configure user lockout defaults
